8a513c8f7a4739daedfd4ce7f233a51a99616c28
impact-eintr <yixingwei4@gmail.com>
2021-03-14 21:10:25 +0800

    nothing

--- C/basic.md
+++ C/basic.md
@@ -8,6 +8,8 @@
 int main()
 {
     int *num = malloc(sizeof(int));
+    *num = 100;
+    printf("%d\n",*num);
     return 0;
 }
 
--- C/basic/hello.c
+++ C/basic/hello.c
@@ -2,9 +2,6 @@
 
 int main()
 {
-    int *num = malloc(sizeof(int));
-    *num = 100;
-    printf("%d\n",*num);
-    return 0;
+    printf("hello\n");
 }
 
