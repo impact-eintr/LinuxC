107066ef7d81f4b8356a9a8bfe10e50ff28f4190
impact-eintr <yixingwei4@gmail.com>
2021-03-15 08:58:52 +0800

    nothing

--- APUE/MPI/mpisum2.c
+++ APUE/MPI/mpisum2.c
@@ -6,8 +6,6 @@
 
 #define N 24
 
-static int sum = 0;
-static int num = 0;
 static int arr[N] = {1,4,3,9,2,8,5,1,1,6,2,7,2,5,0,4,1,8,6,5,1,2,3,9};
 
 int main()
@@ -16,7 +14,8 @@ int main()
     int my_rank;
 
     int *task = malloc(sizeof(int));
-    int res = 0;
+    int *sum = malloc(sizeof(int));
+    int res = 0;//最终结果
     
     MPI_Init(NULL,NULL);
     //从这里开始并行执行
@@ -24,19 +23,26 @@ int main()
     MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
 
     if (my_rank != 0){
+        MPI_Recv(task,sizeof(*task),MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
         int tempsum = 0;
-        for (int i = task;i < task+(N/(comm_sz-1));i++){
+        for (int i = *task;i < *task+(N/(comm_sz-1));i++){
             tempsum += arr[i];
         }
-        sum = tempsum;
-        MPI_Recv(task,sizeof(*task),MPI_INT,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        printf("tempsum = %d\n",tempsum);
+        *sum = tempsum;
+        MPI_Send(sum,sizeof(*sum),MPI_INT,0,0,MPI_COMM_WORLD);
     }else {
-        *sum = 0;
-        for (int i = 1;i < comm_sz;i++){
-            MPI_Send(task,sizeof(*task),MPI_INT,0,0,MPI_COMM_WORLD);
+        for (int i = 0;i < comm_sz;i++){
+            *task = i;
+            MPI_Send(task,sizeof(*task),MPI_INT,i,0,MPI_COMM_WORLD);
+        }
+
+        for (int i = 0;i < comm_sz;i++){
+            MPI_Recv(sum,sizeof(*sum),MPI_INT,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
             res += *sum;
         }
-        printf("%d\n",res);
+        
+        printf("sum = %d\n",res);
     }
     MPI_Finalize();
 
