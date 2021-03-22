2334afb0cdd126e975f25f10c2bc6b6163ebcd11
Impact-yxw <yixingwei4@gmail.com>
2021-03-22 16:16:59 +0800

    nothing

--- Parallel/MPI/HW2_trap/trap/main.c
+++ Parallel/MPI/HW2_trap/trap/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "trap.h"
 
 static double f(double x){
-    return x * x;
+    return sin(x);
 }
 
 int main() {
--- Parallel/MPI/HW2_trap/trap2/main.c
+++ Parallel/MPI/HW2_trap/trap2/main.c
@@ -9,30 +9,29 @@ static double f(double x){
 }
 
 void Get_input(int my_rank, int comm_sz, double* a_p, double* b_p,
-      int* n_p) {
-   int dest;
+            int* n_p) {
+    int dest;
 
-   if (my_rank == 0) {
-       printf("请输入a, b, n:\n");
-       scanf("%lf",a_p);
-       scanf("%lf",b_p);
-       scanf("%d",n_p);
-      for (dest = 1; dest < comm_sz; dest++) {
-         MPI_Send(a_p, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-         MPI_Send(b_p, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-         MPI_Send(n_p, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-      } 
-   } else { /* my_rank != 0 */
-      MPI_Recv(a_p, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,
-            MPI_STATUS_IGNORE);
-      MPI_Recv(b_p, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,
-            MPI_STATUS_IGNORE);
-      MPI_Recv(n_p, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
-            MPI_STATUS_IGNORE);
-   } 
+    if (my_rank == 0) {
+        printf("请输入a, b, n:\n");
+        scanf("%lf",a_p);
+        scanf("%lf",b_p);
+        scanf("%d",n_p);
+        for (dest = 1; dest < comm_sz; dest++) {
+            MPI_Send(a_p, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
+            MPI_Send(b_p, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
+            MPI_Send(n_p, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        } 
+    } else { /* my_rank != 0 */
+        MPI_Recv(a_p, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,
+                    MPI_STATUS_IGNORE);
+        MPI_Recv(b_p, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,
+                    MPI_STATUS_IGNORE);
+        MPI_Recv(n_p, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+                    MPI_STATUS_IGNORE);
+    } 
 }
 
-//如何只输出一句提示
 int main() {
     int my_rank, comm_sz, n , local_n;   
     double a , b , h, local_a, local_b;
