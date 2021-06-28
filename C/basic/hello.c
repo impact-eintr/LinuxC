e7b22c6fa042f8a1eb85450de5cbc0ff76262eaf
impact-eintr <yixingwei4@gmail.com>
2021-06-28 16:31:52 +0800

    merger

--- C/basic/hello.c
+++ C/basic/hello.c
@@ -3,12 +3,9 @@
 
 int main()
 {
-<<<<<<< HEAD
     printf("hello\n");
-=======
     int *num = malloc(sizeof(int));
     *num = 100;
     printf("%d\n",*num);
->>>>>>> d8201497ccb6ce60411ef4f1d6347921a54e7c22
 }
 
