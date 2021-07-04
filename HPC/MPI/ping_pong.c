579dce17a225143d544ea2e5b71d0f0bedf5a71b
PlayWithSanLei <zhanghao63271202@gmail.com>
2021-07-04 21:50:03 +0800

    修改pingpong为clock()

--- HPC/MPI/ping_pong.c
+++ HPC/MPI/ping_pong.c
@@ -1,82 +1,115 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h>
-#include <time.h>
-#include <mpi.h>
+/* for memcpy */ 
+#include <time.h> 
+#include "mpi.h"
 
-#define  MAX_MESG_SIZE 1024*1024
-#define  RES_TEST_ITERS 10
+/* Use these parameters for generating times*/ 
+#define RES_TEST_ITERS 10 
+#define MAX_MESG_SIZE 131072 
+void print_buffer(char mesg[], int mesg_size, int my_rank); 
+double ping_pong(char mesg[], int mesg_size, int iters, MPI_Comm comm, int p, int my_rank);
+double ping_pong_1(char mesg[], int mesg_size, int iters, MPI_Comm comm, int p, int my_rank);
+ /*-------------------------------------------------------------------*/ 
+int main(int argc, char* argv[]) { 
+	int i; 
+	double elapsed, elapsed_1; 
+	char message[MAX_MESG_SIZE]; 
+	char c = 'B'; 
+	MPI_Comm comm;
+	int p;
+	int my_rank;
 
-double ping_pong(char [], int, int, MPI_Comm, int, int);
-void print_buf(char [], int, int );
+	MPI_Init(&argc, &argv);
+	comm = MPI_COMM_WORLD;
+	MPI_Comm_size(comm, &p);
+	MPI_Comm_rank(comm, &my_rank);
+	if (p != 2) {
+		if(my_rank == 0) {
+			fprintf(stderr, "Use two processes. \n");
+		}
+		MPI_Finalize();
+		return 0;
+	}
 
-int main(int argc,char **argv){
-  double elapsed;
-  char message[MAX_MESG_SIZE];
-  char c = 'B';
-  MPI_Comm comm;
-  int comm_size;
-  int my_rank;
-
-  MPI_Init(&argc, &argv);
-  comm = MPI_COMM_WORLD;
-  
-  MPI_Comm_size(comm, &comm_size);
-  MPI_Comm_rank(comm, &my_rank);
-  if (comm_size != 2){
-    if (my_rank == 0){
-      fprintf(stderr, "Please Use Tow process\n");
+	if(my_rank == 0) 
+		c = 'A';
+	for(i = 0; i< MAX_MESG_SIZE;i++)
+		message[i] = c;
+    for(int j = 0; j < 20; j++){
+	    elapsed = ping_pong(message, 0, RES_TEST_ITERS, comm, p, my_rank);
+	    elapsed_1 = ping_pong_1(message, 0, RES_TEST_ITERS, comm, p, my_rank);
+        if(my_rank == 0) {
+	        fprintf(stderr, "iter: %d, Min ping_ping = %8.5e, C_Clock tick = %8.5e\n", i, elapsed/(2*RES_TEST_ITERS), (double)clock());
+	        fprintf(stderr, "iter: %d, Min ping_ping = %8.5e, MPI_Clock tick = %8.5e\n", i, elapsed_1/(2*RES_TEST_ITERS), MPI_Wtick());
+	    }	
     }
-    MPI_Finalize();
-    return 0;
-  }
 
-  if (my_rank == 0){
-    c = 'A';
-  }
 
-  for (int i = 0;i < MAX_MESG_SIZE;i++){
-    message[i] = c;
-  }
-
-  elapsed = ping_pong(message, 1024*1024, RES_TEST_ITERS, comm, comm_size, my_rank);
-  if (my_rank == 0){
-    fprintf(stderr, "Min ping_pong = %8.5e, Clock tick = %8.5e\n",
-            elapsed/(2 * RES_TEST_ITERS), MPI_Wtick());
-  }
-  MPI_Finalize();
-  exit(0);
-
+	MPI_Finalize();
+	return 0;
 }
 
-double ping_pong(char *msg, int msg_size, int iters, MPI_Comm comm, int comm_size, int my_rank){
-  MPI_Status status;
-  double start;
+/*------------------------------- ~@~T---------*/
+double ping_pong(char mesg[], int mesg_size, int iters, MPI_Comm comm,
+                int p, int my_rank){
+        int i;
+        MPI_Status status;
+        clock_t start;
 
-  if (my_rank == 0){
-    start = MPI_Wtime();
-    for (int i = 0;i < iters;i++){
-      MPI_Send(msg, msg_size, MPI_CHAR, 1, 0, comm);
-      MPI_Recv(msg, msg_size, MPI_CHAR, 1, 0, comm, &status);
-    }
-    return MPI_Wtime() - start;
-  }else if (my_rank == 1){
-    for (int i = 0;i < iters;i++){
-      MPI_Recv(msg, msg_size, MPI_CHAR, 0, 0, comm, &status);
-      //print_buf(msg, msg_size, 1);
-      MPI_Send(msg, msg_size, MPI_CHAR, 0, 0, comm);
-    }
-  }
+        if (my_rank == 0) {
+                start = clock();
+                for(i=0; i<iters; i++){
+                        MPI_Send(mesg, mesg_size, MPI_CHAR,1,0, comm);
+                        MPI_Recv(mesg, mesg_size, MPI_CHAR,1,0, comm, &status);
+                }
+                double total, end_t;
+                end_t = clock();
+                total = (double)(end_t - start) / CLOCKS_PER_SEC;
+                return total;
+        }else if(my_rank == 1){
+                for (i= 0; i<iters; i++){
+                        MPI_Recv(mesg, mesg_size,MPI_CHAR,0,0, comm, &status);
+                        print_buffer(mesg, mesg_size,1);
+                        MPI_Send(mesg, mesg_size, MPI_CHAR,0,0, comm);
+                }
+        }
+        return 0.0;
+}/* ping _pong */
 
-  return 0.0;
+double ping_pong_1(char mesg[], int mesg_size, int iters, MPI_Comm comm,
+                int p, int my_rank){
+        int i;
+        MPI_Status status;
+        clock_t start;
+
+        if (my_rank == 0) {
+                start = MPI_Wtime();
+                for(i=0; i<iters; i++){
+                        MPI_Send(mesg, mesg_size, MPI_CHAR,1,0, comm);
+                        MPI_Recv(mesg, mesg_size, MPI_CHAR,1,0, comm, &status);
+                }
+                return MPI_Wtime() - start;
+        }else if(my_rank == 1){
+                for (i= 0; i<iters; i++){
+                        MPI_Recv(mesg, mesg_size,MPI_CHAR,0,0, comm, &status);
+                        print_buffer(mesg, mesg_size,1);
+                        MPI_Send(mesg, mesg_size, MPI_CHAR,0,0, comm);
+                }
+        }
+        return 0.0;
+}/* ping _pong */
+
+
+/*-----------------------------------_*/
+void print_buffer(char mesg[],int mesg_size,int my_rank){
+        char temp[MAX_MESG_SIZE + 1];
+
+
+        memcpy(temp, mesg, mesg_size);
+        temp[mesg_size]='\0';
+        //printf("Process %d > %s\n", my_rank, temp);
+        fflush(stdout);
 }
-
-void print_buf(char msg[], int msg_size,int my_rank){
-  char temp[MAX_MESG_SIZE + 1];
-
-  memcpy(temp, msg, msg_size);
-  temp[msg_size] = '\0';
-  printf("[%d] > %s\n",my_rank, temp);
-  fflush(stdout);
-
-}
+/*print buffer*/
