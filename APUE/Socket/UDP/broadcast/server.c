83ac6f4056d7c777eb04614c61724589e4de84d4
impact-eintr <yixingwei4@gmail.com>
2021-11-23 18:05:12 +0800

    test

--- APUE/Socket/UDP/broadcast/server.c
+++ APUE/Socket/UDP/broadcast/server.c
@@ -7,72 +7,59 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <stdarg.h>
+#include <math.h>
+#include <string.h>
 
 #include "proto.h"
 
-#define IPSIZE 1024
 #define SERVERPORT "2333"
-
-static void debug(char *fmt,...){
-    va_list ap;
-    va_start(ap,fmt);
-
-    printf("DEBUG ");
-    printf(fmt,va_arg(ap,int));
-
-    va_end(ap);
-}
 
 int main()
 {
-    int sfd;
-    struct sockaddr_in laddr;//local addr
-    struct sockaddr_in raddr;//remote addr
-    struct msg_st *rbuf;
-    char ip[IPSIZE];
+  int sfd;
+  struct msg_st *sbuf;
+  struct sockaddr_in raddr;//remote addr
 
-    int pkglen = sizeof(struct msg_st)+NAMEMAX;
-    debug("%d\n",pkglen);
-    rbuf = malloc(pkglen);
-    if (rbuf == NULL){
-        perror("malloc()");
-        exit(1);
+  sfd = socket(AF_INET,SOCK_DGRAM,0/*IPPROTO_UDP*/);
+  if(sfd < 0){
+    perror("socket()");
+    exit(1);
+  }
+  //设置socket的属性
+  int val = 1;
+  if (setsockopt(sfd,SOL_SOCKET,SO_BROADCAST,&val,sizeof(val)) < 0){
+    perror("setsockopt()");
+    exit(1);
+  }//打开广播属性
+
+
+  int pkglen = sizeof(struct msg_st)+strlen("Mike")+1;// 注意给'/0'留位置
+  sbuf = malloc(pkglen);
+  if (sbuf == NULL){
+    perror("malloc()");
+    exit(1);
+  }
+
+  char *name = "Mike";
+  strcpy(sbuf->name,name);
+  sbuf->math = htonl(rand()%100);//主机字节序转网络字节序
+  sbuf->chinese = htonl(rand()%100);
+
+  raddr.sin_family = AF_INET;
+  raddr.sin_port = htons(atoi(SERVERPORT));
+  inet_pton(AF_INET,"192.168.47.255",&raddr.sin_addr);
+
+  while(1) {
+    if(sendto(sfd,sbuf,pkglen,0,(void *)&raddr,sizeof(raddr)) < 0){
+      perror("sendto()");
+      exit(1);
     }
 
-    sfd = socket(AF_INET,SOCK_DGRAM,0/*IPPROTO_UDP*/);
-    if (sfd < 0){
-        perror("socket()");
-        exit(1);
-    }
+    puts("OK");
+    sleep(1);
+  }
 
-    //设置socket属性
-    int val = 1;
-    if(setsockopt(sfd,SOL_SOCKET,SO_BROADCAST,&val,sizeof(val)) < 0){
-        perror("setsockopt()");
-        exit(1);
-    }
+  close(sfd);
 
-    laddr.sin_family = AF_INET;//指定协议
-    laddr.sin_port = htons(atoi(SERVERPORT));//指定网络通信端口
-    inet_pton(AF_INET,"0.0.0.0",&laddr.sin_addr);//IPv4点分式转二进制数
-
-    if(bind(sfd,(void *)&laddr,sizeof(laddr)) < 0){
-        perror("bind()");
-        exit(1);
-    }
-
-    socklen_t raddr_len = sizeof(raddr);
-    while(1){
-        recvfrom(sfd,rbuf,pkglen,0,(void *)&raddr,&raddr_len);//报式套接字每次通信都需要知道对方是谁
-        inet_ntop(AF_INET,&raddr.sin_addr,ip,IPSIZE);
-        printf("%s %d\n",ip,ntohs(raddr.sin_port));
-        printf("%s %d %d\n",rbuf->name,ntohl(rbuf->math),ntohl(rbuf->chinese));
-        fflush(NULL);
-    }
-
-    close(sfd);
-    
-    exit(0);
+  exit(0);
 }
-
