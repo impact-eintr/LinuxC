92a1158a3f8f8b69b2c1fecd6bc877ae100c3122
impact-eintr <yixingwei4@gmail.com>
2022-05-06 20:35:48 +0800

    address.h memory.h

---
+++ CSAPP/src/csapp/headers/address.h
@@ -0,0 +1,73 @@
+#ifndef ADDRESS_H_
+#define ADDRESS_H_
+
+#include <stdint.h>
+
+#define SRAM_CACHE_INDEX_LENGTH (6)
+#define SRAM_CACHE_OFFSET_LENGTH (6)
+#define SRAM_CACHE_TAG_LENGTH (40)
+
+#define PHYSICAL_PAGE_OFFSET_LENGTH (12)
+#define PHYSICAL_PAGE_NUMBER_LENGTH (4)
+#define PHYSICAL_ADDRESS_LENGTH (16)
+
+#define VIRTUAL_PAGE_OFFSET_LENGTH (12)
+#define VIRTUAL_PAGE_NUMBER_LENGTH (9) // 9 + 9 + 9 + 9 = 36
+#define VIRTUAL_ADDRESS_LENGTH (48)
+
+#define TLB_CACHE_OFFSET_LENGTH (12)
+#define TLB_CACHE_INDEX_LENGTH (4)
+#define TLB_CACHE_TAG_LENGTH (32)
+
+/*
++--------+--------+--------+--------+---------------+
+|  VPN1  |  VPN2  |  VPN3  |  VPN4  |               |
++--------+--------+--------+-+------+      VPO      |
+|    TLBT                    | TLBI |               |
++---------------+------------+------+---------------+
+                |        PPN        |      PPO      |
+                +-------------------+--------+------+
+                |        CT         |   CI   |  CO  |
+                +-------------------+--------+------+
+*/
+typedef union {
+  uint64_t address_value;
+
+  // physical address: 52
+  struct {
+    union {
+      uint64_t paddr_value : PHYSICAL_ADDRESS_LENGTH;
+      struct {
+        uint64_t ppo : PHYSICAL_PAGE_OFFSET_LENGTH;
+        uint64_t ppn : PHYSICAL_PAGE_NUMBER_LENGTH;
+      };
+    };
+  };
+  // sram cache: 52
+  struct {
+    uint64_t co : SRAM_CACHE_OFFSET_LENGTH;
+    uint64_t ci : SRAM_CACHE_INDEX_LENGTH;
+    uint64_t ct : SRAM_CACHE_TAG_LENGTH;
+  };
+  // virtual cache: 48
+  struct {
+    union {
+      uint64_t vaddr_value : VIRTUAL_ADDRESS_LENGTH;
+      struct {
+        uint64_t vpo : VIRTUAL_PAGE_OFFSET_LENGTH;
+        uint64_t vpn4 : VIRTUAL_PAGE_NUMBER_LENGTH;
+        uint64_t vpn3 : VIRTUAL_PAGE_NUMBER_LENGTH;
+        uint64_t vpn2 : VIRTUAL_PAGE_NUMBER_LENGTH;
+        uint64_t vpn1 : VIRTUAL_PAGE_NUMBER_LENGTH;
+      };
+    };
+  };
+  // TLB cache: 48
+  struct {
+    uint64_t tlbo : TLB_CACHE_OFFSET_LENGTH; // virtual page offset
+    uint64_t tlbi : TLB_CACHE_INDEX_LENGTH;  // TLB set index
+    uint64_t tlbt : TLB_CACHE_TAG_LENGTH;    // TLB line tag
+  };
+} address_t;
+
+#endif // ADDRESS_H_
--- CSAPP/src/csapp/headers/memory.h
+++ CSAPP/src/csapp/headers/memory.h
@@ -8,6 +8,16 @@
 
 uint8_t pm[PHYSICAL_MEMORY_SPACE];
 
+// page table struct
+
+typedef union {
+  uint64_t pte_value;
+} pte123_t; // 8 bytes = 64bits
+
+typedef union {
+  uint64_t pte_value;
+} pte4_t; // 8 bytes = 64bits
+
 /*======================================*/
 /*      memory R/W                      */
 /*======================================*/
@@ -18,4 +28,6 @@ void cpu_write64bits_dram(uint64_t paddr, uint64_t data);
 void cpu_readinst_dram(uint64_t paddr, char *buf);
 void cpu_writeinst_dram(uint64_t paddr, const char *str);
 
+void bus_read_cacheline(uint64_t paddr, uint8_t *block);
+void bus_write_cacheline(uint64_t paddr, uint8_t *block);
 #endif // MEMORY_H_
