80d28b73e130a324e3818c50d1c0fe817c4aba20
impact-eintr <yixingwei4@gmail.com>
2022-06-15 21:34:08 +0800

    bst integrated framework

--- CSAPP/src/csapp/algorithm/bst.c
+++ CSAPP/src/csapp/algorithm/bst.c
@@ -210,3 +210,145 @@ uint64_t bst_internal_find_succ(rbtree_internal_t *tree,
   // if no node key <= target key, return NULL_ID
   return successor; /// reutnr supremum(上确界)
 }
+
+static void bst_internal_dfs_print(uint64_t node, rbtree_node_interface *i_node,
+                                   int depth) {
+  rbt_validate_interface(i_node, IRBT_CHECKNULL | IRBT_LEFT | IRBT_RIGHT |
+                                     IRBT_KEY | IRBT_COLOR);
+
+  if (depth >= 10) {
+    // meanless to print depth >= 10
+    printf("*");
+    return;
+  }
+
+  if (i_node->is_null_node(node) == 1) {
+    printf("#");
+    return;
+  }
+
+  if (i_node->get_color(node) == COLOR_RED) {
+    printf("(0x%lx/%lu:\033[31m%lu\033[0m,", node, node, i_node->get_key(node));
+  } else {
+    printf("(0x%lx/%lu:%lu,", node, node, i_node->get_key(node));
+  }
+
+  bst_internal_dfs_print(i_node->get_leftchild(node), i_node, depth + 1);
+  printf(",");
+  bst_internal_dfs_print(i_node->get_rightchild(node), i_node, depth + 1);
+  printf(")");
+}
+
+void bst_internal_prinf(uint64_t node, rbtree_node_interface *i_node) {
+  bst_internal_dfs_print(node, i_node, 0);
+  printf("\n");
+}
+
+
+// -------------------------------------
+//       For Unit Test
+// -------------------------------------
+void internal_tree_construct_keystr(rbtree_internal_t *tree,
+                                    rbtree_node_interface *i_node, char *str) {}
+
+uint internal_tree_compare(uint64_t a, uint64_t b, rbtree_node_interface *i_node, int is_rbt) {
+
+}
+
+static void rbt_verify_dfs(uint64_t p, uint64_t *black_height,
+                           uint64_t *key_min, uint64_t *key_max,
+                           rbtree_node_interface *i_node, int is_rbt) {}
+
+void rbt_internal_verify(rbtree_internal_t *tree, rbtree_node_interface *i_node,
+                         int is_rbt) {}
+
+// =====================================
+//         Default Implementation
+// =====================================
+//Implementation of BST node access
+static int is_null_node(uint64_t node_id) {}
+
+static uint64_t construct_node() {}
+
+static int destruct_node(uint64_t node_id) {}
+
+static int compare_nodes(uint64_t first, uint64_t second) {}
+
+static uint64_t get_parent(uint64_t node_id) {}
+
+static int set_parent(uint64_t node_id, uint64_t parent_id) {}
+
+static uint64_t get_leftchild(uint64_t node_id) {}
+
+static int set_leftchild(uint64_t node_id, uint64_t left_id) {}
+
+static uint64_t get_rightchild(uint64_t node_id) {}
+
+static int set_rightchild(uint64_t node_id, uint64_t right_id) {}
+
+static rb_color_t get_color(uint64_t node_id) {}
+
+static int set_color(uint64_t node_id, rb_color_t color) {}
+
+static uint64_t get_key(uint64_t node_id) {}
+
+static int set_key(uint64_t node_id, uint64_t key) {}
+
+static uint64_t get_value(uint64_t node_id) {}
+
+static int set_value(uint64_t node_id, uint64_t value) {}
+
+// shared with red-black tree
+rbtree_node_interface default_i_rbt_node = {
+    .construct_node = &construct_node,
+    .destruct_node = &destruct_node,
+    .is_null_node = &is_null_node,
+    .compare_nodes = &compare_nodes,
+    .get_parent = &get_parent,
+    .set_parent = &set_parent,
+    .get_leftchild = &get_leftchild,
+    .set_leftchild = &set_leftchild,
+    .get_rightchild = &get_rightchild,
+    .set_rightchild = &set_rightchild,
+    .get_color = &get_color,
+    .set_color = &set_color,
+    .get_key = &get_key,
+    .set_key = &set_key,
+    .get_value = &get_value,
+    .set_value = &set_value,
+};
+
+// child class of base class
+static int update_root(rbtree_internal_t *this, uint64_t new_root) {}
+
+// ----------------------------
+//  The exposed interfaces
+// ----------------------------
+
+// constructor and destructor
+rb_tree_t *bst_construct() {}
+
+// for test use
+rb_tree_t *bst_construct_keystr(char *str) {}
+
+void bst_print(rb_node_t *node) {}
+
+static void bst_destruct_subtree(uint64_t root) {}
+
+void bst_free(rb_tree_t *tree) {}
+
+void bst_add(rb_tree_t *tree, uint64_t key) {}
+
+void bst_insert(rb_tree_t *tree, rb_node_t *node) {}
+
+void bst_remove(rb_tree_t *tree, uint64_t key) {}
+
+void bst_delete(rb_tree_t *tree, rb_node_t *node) {}
+
+rb_node_t *bst_find(rb_tree_t *tree, uint64_t key) {}
+
+rb_node_t *bst_find_succ(rb_tree_t *tree, uint64_t key) {}
+
+int bst_compare(rb_tree_t *a, rb_tree_t *b) {}
+
+void bst_validate(rb_tree_t *tree) {}
